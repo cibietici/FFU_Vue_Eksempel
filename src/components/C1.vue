911f881db7b40eff967decd62f6c8058c627be87
carlo alberto burato <carloalberto.burato@gmail.com>
2022-02-16 08:35:13 +0100

    Add new props

--- src/components/C1.vue
+++ src/components/C1.vue
@@ -1,3 +1,14 @@
 <template>
-    <h1>C1</h1>
+    <h1>{{ title ? title : 'C1'}}</h1>
 </template>
+<script>
+export default {
+  name: 'C1',
+  props: {
+      title: {
+          type: String,
+          default: 'title'
+      }
+  }
+}
+</script>
--- src/views/Comps.vue
+++ src/views/Comps.vue
@@ -1,5 +1,5 @@
 <template>
-    <General compName="C1" />
+    <General compName="C2" />
 </template>
 <script>
 import General from '../components/General.vue';
